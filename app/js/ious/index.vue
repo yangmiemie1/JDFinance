e9185f2f2063b01a4a1b45824103310b5f61fa77
yangmiemie1 <11094759234@qq.com>
2019-08-16 14:52:46 +0800

    ious

---
+++ app/js/ious/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Heador/>
+    <Slider/>
+    <Weifare/>
+    <More/>
+    <Footer/>
+    <Navbar/>
+  </div>
+</template>
+
+<script>
+import Heador from '../public/header.vue';
+import Footer from '../public/footer.vue';
+import Navbar from '../public/navbar.vue';
+import Slider from './islider.vue';
+import Weifare from './weifare.vue';
+import More from './more.vue';
+
+  export default {
+    components:{
+      Heador,
+      Slider,
+      Footer,
+      Navbar,
+      Weifare,
+      More
+    }
+  }
+</script>
+
+<style lang="scss">
+@import '../../css/reset.scss';
+  
+</style>
--- app/js/router/index.js
+++ app/js/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
 import Money from "../money/index.vue"
-
+import Ious from "../ious/index.vue"
 
 Vue.use(Router)
 
@@ -17,6 +17,11 @@ export default new Router({
             path: "/money",
             name: "money",
             component: Money,
+        },
+        {
+            path: "/ious",
+            name: "ious",
+            component: Ious,
         },
     ],
 })
